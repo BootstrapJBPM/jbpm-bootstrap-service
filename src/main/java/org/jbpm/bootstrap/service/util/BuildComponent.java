9ac54c521e208c8cf7ac70320821a8391832da1d
Maciej Swiderski <swiderski.maciej@gmail.com>
2019-02-26 11:13:55 +0100

    updated certificate and support for multiple versions of the spring boot dependencies based on kie version

--- src/main/java/org/jbpm/bootstrap/service/util/BuildComponent.java
+++ src/main/java/org/jbpm/bootstrap/service/util/BuildComponent.java
@@ -37,6 +37,9 @@ public class BuildComponent {
     private static final String KIE_VERSION = System.getProperty("org.kie.version",
                                                                  DEFAULT_VERSION);
     private static final String MVN_SETTINGS = System.getProperty("kie.maven.settings.custom");
+    
+    private static final String DEFAULT_SPRING_BOOT_VERSION = "2.1.1.RELEASE";
+    private static final String OLD_SPRING_BOOT_VERSION = "1.5.12.RELEASE";
 
     @Autowired
     private ProcessService processService;
@@ -100,6 +103,11 @@ public class BuildComponent {
             params.put("mavenSettings",
                        mavenSettings);
             params.put("generationType", isWeb ? "web" : "rest");
+            
+            String[] versionItems = project.getVersion().split("\\.");
+            Integer minorVersion = Integer.parseInt(versionItems[1]);
+            // in 7.18 spring boot was upgraded to 2.1.x and thus needs to change version based on selected version
+            params.put("springbootVersion", minorVersion >= 18 ? DEFAULT_SPRING_BOOT_VERSION : OLD_SPRING_BOOT_VERSION);
 
             long processInstanceId = processService.startProcess(CONTAINER_ID,
                                                                  PROCESS_ID,
