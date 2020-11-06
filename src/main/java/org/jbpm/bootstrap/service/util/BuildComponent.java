e705142f32315fb4c388c0846b8b3bdaa8b5d5a2
Tihomir Surdilovic <tsurdilo@redhat.com>
2020-11-05 21:52:46 -0500

    Add new community and product versions
    Signed-off-by: Tihomir Surdilovic <tsurdilo@redhat.com>

--- src/main/java/org/jbpm/bootstrap/service/util/BuildComponent.java
+++ src/main/java/org/jbpm/bootstrap/service/util/BuildComponent.java
@@ -33,7 +33,7 @@ public class BuildComponent {
     private static final String CONTAINER_ID = "jbpm-bootstrap-kjar";
     private static final String PROCESS_ID = "GenerateProject";
 
-    private static final String DEFAULT_VERSION = "7.41.0.Final";
+    private static final String DEFAULT_VERSION = "7.45.0.Final";
     private static final String KIE_VERSION = System.getProperty("org.kie.version",
                                                                  DEFAULT_VERSION);
     private static final String MVN_SETTINGS = System.getProperty("kie.maven.settings.custom");
